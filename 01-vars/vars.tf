d1d47d2ea1b159096d5fa8a7bd774fd16831e6b7
sruthilaya <sruthilayagadila@gmail.com>
2023-07-01 15:43:06 +0530

    terraform

--- 01-vars/vars.tf
+++ 01-vars/vars.tf
@@ -12,4 +12,55 @@ output "sample" {
 
 output "sample1" {
   value = var.sample1
+
+}
+
+# plain
+
+variable "course" {
+  default = "devops traning"
+}
+
+output "course" {
+  value = var.course
+}
+
+#list
+variable "courses" {
+  default = [
+  "devops",
+  "aws",
+  "python"
+  ]
+}
+
+output "courses" {
+  value = var.courses
+}
+
+output "courses" {
+  value = var.courses[2]
+}
+
+#map
+
+variable "course-details" {
+  default = {
+  devops = {
+      name="devops"
+      timing="7.am"
+    }
+  aws = {
+      name= "aws"
+      timing="6am"
+    }
+  }
+}
+
+output "course-details" {
+  value = var.course-details
+}
+
+output "course-details" {
+  value = var.course-details["devops"]
 }
