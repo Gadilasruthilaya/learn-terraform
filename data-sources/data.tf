53aaf56e056f82e9f66fe374f36c376b8e8235cd
sruthilaya <sruthilayagadila@gmail.com>
2023-07-02 16:09:14 +0530

    terraform

--- data-sources/data.tf
+++ data-sources/data.tf
@@ -1,8 +1,7 @@
 data "aws_ami" "example" {
-  executable_users =["973714476881"]
+  owners =["973714476881"]
   most_recent      = true
   name_regex       = "Centos-8-DevOps-Practice"
-  owners           = ["self"]
 
 }
 
