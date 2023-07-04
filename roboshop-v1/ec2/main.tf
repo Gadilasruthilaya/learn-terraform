c77c2ea699e0c1644072254a918d23caaa9d7993
sruthilaya <sruthilayagadila@gmail.com>
2023-07-04 21:31:35 +0530

    roboshop-v1

--- roboshop-v1/ec2/main.tf
+++ roboshop-v1/ec2/main.tf
@@ -47,7 +47,7 @@ resource "aws_security_group" "sg" {
   ingress {
     from_port        = 0
     to_port          = 0
-    protocol         = "-"
+    protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
 
   }
