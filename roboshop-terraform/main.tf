0c54e40ab53a57190f6e7690f6de8b4fc7dd2e90
sruthilaya <sruthilayagadila@gmail.com>
2023-12-17 18:57:46 +0530

    terraform

--- roboshop-terraform/main.tf
+++ roboshop-terraform/main.tf
@@ -6,16 +6,16 @@ module "instances" {
 
 variable "instances" {
   default = {
-    frontend = {},
-    mongodb={},
-    catalogue={},
-    redis={},
-    user={},
-    cart={},
-    mysql={},
-    shipping={},
-    rabbitmq={},
-    payment={},
+    frontend = {}
+    mongodb={}
+    catalogue={}
+    redis={}
+    user={}
+    cart={}
+    mysql={}
+    shipping={}
+    rabbitmq={}
+    payment={}
     dispatch={}
   }
 }
